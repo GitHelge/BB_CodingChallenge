c11fb90ed074978fd7032408eadd26bffe58f461
Christian Bauer <christianbauerhh@gmail.com>
2020-12-11 22:24:13 +0100

    Add dotenv dependency

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+Future main() async {
+  await DotEnv().load('.env');
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    final apiKey = DotEnv().env['OMDB_API_KEY'];
+    print('OMDB_API_KEY=' + apiKey.toString());
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
